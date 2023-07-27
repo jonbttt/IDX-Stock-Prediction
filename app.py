316db61fefbf06d67a796028b049935161c16587
jonbttt <48402029+jonbttt@users.noreply.github.com>
2023-07-27 15:55:12 +0700

    update app.py

--- app.py
+++ app.py
@@ -24,15 +24,14 @@ def get_st_button_a_tag(url_link, button_name):
     return f'''
     <a href={url_link}><button style="
     fontWeight: 400;
-    fontColor: #FAFAFA;
     padding: 0.25rem 0.75rem;
     borderRadius: 0.25rem;
     margin: 0px;
     lineHeight: 1.6;
     width: auto;
     userSelect: none;
-    backgroundColor: #FF4B4B;
-    border: 1px solid rgba(255, 75, 75, 0.2);">{button_name}</button></a>
+    backgroundColor: #FAFAFA;
+    border: 1px solid rgba(49, 51, 63, 0.2);">{button_name}</button></a>
     '''
 
 apikey = st.secrets["apikey"]
@@ -125,7 +124,7 @@ with st.sidebar:
 
   with tab3:
     st.write("Recent Finance News")
-    st.caption("Articles are from the official IDX channel")
+    st.caption("Articles are obtained from the official IDX channel")
     string1 = ""
     newsurl = "https://www.idxchannel.com/indeks?date="+today+"&idkanal=1"
     print(newsurl)
